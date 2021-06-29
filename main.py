672bb2c316c6e599a145827b537d4c9c4651d522
Leevi Laitala <leevi.laitala@protonmail.com>
2021-06-29 19:57:33 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+import argparse
+import subprocess
+import pulsectl
+
+icons     = ['volume_mute', 'volume_down', 'volume_up', 'volume_off'] # Last icon is used for mute icon
+width     = 20 
+charEmpty = '_' 
+charFull  = '█'
+charMute  = '░'
+step      = 0.05
+
+notifyTimeout = 3000
+notifyID      = 2593
+
+parser = argparse.ArgumentParser(description='pyvolctrl')
+parser.add_argument('action',    choices=['inc', 'dec', 'togglemute'])
+parser.add_argument('--step',    type=int, help='How many percent volume is to be changed (default: 5)')
+parser.add_argument('--width',   type=int, help='Full width of the bar (default: 20)')
+parser.add_argument('--timeout', type=int, help='Notification timeout in milliseconds (default: 3000)')
+args = parser.parse_args()
+
+if args.step: step = abs(args.step) / 100
+if args.width: width = abs(args.width)
+if args.timeout: notifyTimeout = abs(args.timeout)
+
+with pulsectl.Pulse('') as pulse:
+    sink = 0
+    
+    for i in pulse.sink_list():
+        if i.name == pulse.server_info().default_sink_name:
+            sink = i
+    
+    muted = int(sink.mute)
+    percentage = round(sink.volume.value_flat * 100)
+
+    if args.action == 'inc':  pulse.volume_change_all_chans(sink, +step)
+    if args.action == 'dec':  pulse.volume_change_all_chans(sink, -step)
+    if args.action == 'togglemute': 
+        if muted == 0:
+            pulse.mute(sink, mute=True)
+        else:
+            pulse.mute(sink, mute=False)
+
+        muted = 1 - muted # Mute state was changes, so invert
+
+def icon():
+    arrlen = len(icons) - 1 # -1 because last icon is used for mute
+    clamped = int(percentage / (100 / arrlen))
+
+    if muted: return icons[len(icons) - 1]
+    else:     return icons[clamped]
+
+def barStr():
+    retStr=""
+
+    for i in range(0, width):
+        if i < int(percentage * width / 100):
+            if muted: retStr += charMute
+            else:     retStr += charFull
+        else:
+            retStr += charEmpty
+
+    return retStr
+
+subprocess.run('dunstify -i %s -t %s -r %s -u low "%s"' % (icon(), notifyTimeout, notifyID, barStr()), shell=True)
